d4ad21492cd0c85cfb0e46682b5693c7e82b9fe2
wahnkoij <sophiehoka@gmail.com>
2025-02-20 21:42:51 +0300

    postgresql via pqxx set

---
+++ build/CMakeFiles/database_manager_app.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wahnkoij/Documents/project/MoneyMaze/main.cpp" "CMakeFiles/database_manager_app.dir/main.cpp.o" "gcc" "CMakeFiles/database_manager_app.dir/main.cpp.o.d"
+  "/home/wahnkoij/Documents/project/MoneyMaze/src/database/DatabaseManager.cpp" "CMakeFiles/database_manager_app.dir/src/database/DatabaseManager.cpp.o" "gcc" "CMakeFiles/database_manager_app.dir/src/database/DatabaseManager.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/database_manager_app.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for database_manager_app.
+# This may be replaced when dependencies are built.
