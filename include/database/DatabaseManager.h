08fbc6eaac5cb38fe82efadecebb8f4a29e390a7
alinatomilova <tomilova006@bk.ru>
2025-03-08 12:55:23 +0300

    Create DatabaseManager.h

---
+++ include/database/DatabaseManager.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "db_config.h"
+#include <pqxx/pqxx>
+
+enum class LogLevel { DEBUG, INFO, WARNING, ERROR };
+
+class DatabaseManager {
+public:
+  DatabaseManager();
+  ~DatabaseManager();
+
+  bool connect();
+  bool disconnect();
+
+  // Методы для работы с пользователями
+  bool createUser(const std::string &username, const std::string &password);
+  bool authenticateUser(const std::string &username,
+                        const std::string &password);
+
+  // Методы для работы с финансовым профилем
+  enum class FinancialProfile {
+    BALANCE,
+    DAILY_MINIMUM,
+    FINANCIAL_WELL_BEING,
+    DEBT,
+    UNKNOWN_COLUMN
+  };
+
+  FinancialProfile financial_profile_column(const std::string &column_name) {
+    if (column_name == "balance") {
+      return FinancialProfile::BALANCE;
+    } else if (column_name == "daily minimum" ||
+               column_name == "daily_minimum") {
+      return FinancialProfile::DAILY_MINIMUM;
+    } else if (column_name == "dept") {
+      return FinancialProfile::DEBT;
+    } else if (column_name == "financial well-being" ||
+               column_name == "financial well being" ||
+               column_name == "financial_well_being") {
+      return FinancialProfile::FINANCIAL_WELL_BEING;
+    } else {
+      return FinancialProfile::UNKNOWN_COLUMN;
+    }
+  }
+
+  bool create_financial_profile(int user_id, double initial_balance,
+                                double daily_minimum,
+                                double financial_well_being, double debt);
+  bool update_financial_profile(const std::string &column_name, int user_id,
+                                double new_value);
+  double get_value_from_financial_profile(const std::string &column_name,
+                                          int user_id);
+
+private:
+  std::unique_ptr<pqxx::connection> conn;
+};
