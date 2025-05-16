c351107bb224c6c470a954a840d33e73a1b3f33b
alinatomilova <tomilova006@bk.ru>
2025-05-16 14:36:37 +0300

    Add files via upload

--- Desktop/src/reg_window.cpp
+++ Desktop/src/reg_window.cpp
@@ -2,12 +2,14 @@
 #include "ui_reg_window.h"
 #include "http_client.h"
 
-
 reg_window::reg_window(QWidget *parent)
-    : QDialog(parent), ui(new Ui::reg_window)
+    : QDialog(parent), ui(new Ui::reg_window), http_client(new httpClient(this))
 {
     ui->setupUi(this);
     this->setWindowTitle("Регистрация");
+
+    connect(http_client, &httpClient::registration_finished, this, &reg_window::handle_registration_result);
+    connect(http_client, &httpClient::error_occurred, this, &reg_window::handle_network_error);
 
 
     ui->pushButton_create_account->setStyleSheet(
@@ -32,9 +34,12 @@ reg_window::reg_window(QWidget *parent)
 }
 
 
-reg_window::~reg_window() { delete ui; }
+reg_window::~reg_window() {
+    delete ui;
+}
 
-void reg_window::err_message(const QString& message) {
+void reg_window::show_message(const QString &message, int message_type)
+{
     const int timeout_ms = 2500;
 
     QLabel* label = new QLabel(this);
@@ -42,14 +47,27 @@ void reg_window::err_message(const QString& message) {
     label->setText(message);
     label->setAlignment(Qt::AlignCenter);
 
-    label->setStyleSheet(
-        "background-color: rgba(216, 58, 47, 0.9);"
-        "color: rgb(255, 255, 255);"
-        "border-radius: 5px;"
-        "padding: 10px;"
-        "font-family: 'Gill Sans', sans-serif;"
-        "font-size: 14px;"
-        );
+    if (message_type == ERROR){
+        label->setStyleSheet(
+            "background-color: rgba(216, 58, 47, 0.9);"
+            "color: rgb(255, 255, 255);"
+            "border-radius: 5px;"
+            "padding: 10px;"
+            "font-family: 'Gill Sans', sans-serif;"
+            "font-size: 14px;"
+            );
+    } else if (message_type == SUCCESS){
+        label->setStyleSheet(
+            "background-color: rgba(97, 197, 84, 0.9);"
+            "color: rgb(255, 255, 255);"
+            "border-radius: 5px;"
+            "padding: 10px;"
+            "font-family: 'Gill Sans', sans-serif;"
+            "font-size: 14px;"
+            );
+    } else {
+        return;
+    }
 
     // Чтобы сообщение было посередине окна
     int x = (this->width() - label->sizeHint().width()) / 2;
@@ -61,6 +79,24 @@ void reg_window::err_message(const QString& message) {
     QTimer::singleShot(timeout_ms, label, &QLabel::deleteLater);
 }
 
+void reg_window::handle_registration_result(int status_code){
+    if (status_code == 200){
+        show_message("Вы успешно зарегестрировались!", SUCCESS);
+
+        QTimer::singleShot(2500 + 200, this, [this]{
+            this->hide();
+        });
+
+    }
+    else {
+        show_message("Ошибка регистрации", ERROR);
+    }
+}
+
+
+void reg_window::handle_network_error(const QString &error){
+    show_message("Ошибка сети: " + error, ERROR);
+}
 
 
 void reg_window::on_lineEdit_login_textEdited(const QString &arg1)
@@ -98,30 +134,24 @@ QString reg_window::get_confirmation()
 
 bool reg_window::check_password_confirmation()
 {
-    return (get_new_password() == get_confirmation());
+    return (get_password() == get_confirmation());
 }
 
 
 void reg_window::on_pushButton_create_account_clicked() {
-    if (get_new_login().length() < 3) {
-        err_message("Логин должен содержать минимум 3 символа");
+    if (get_nickname().length() < 3) {
+        show_message("Логин должен содержать минимум 3 символа", ERROR);
         return;
     }
-    if (get_new_password().length() < 6){
-        err_message("Пароль должен содержать минимум 6 символов");
+    if (get_password().length() < 6){
+        show_message("Пароль должен содержать минимум 6 символов", ERROR);
         return;
     }
     if (!check_password_confirmation()){
-        err_message("Подтверждение пароля не совпадает с исходным");
+        show_message("Подтверждение пароля не совпадает с исходным", ERROR);
         return;
     }
 
-    const int status_code = registrate(get_nickname(), get_password());
 
-    if (status_code != 0){
-        err_message("Произошла ошибка");
-        return;
-    }
-
-    this->hide();
+    http_client->registrate(get_nickname(), get_password());
 }
