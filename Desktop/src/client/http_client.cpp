81348e48852514978c3959c9714c1421f9c1aaa9
alinatomilova <tomilova006@bk.ru>
2025-05-16 12:33:39 +0300

    Add files via upload

--- Desktop/src/client/http_client.cpp
+++ Desktop/src/client/http_client.cpp
@@ -1,38 +1,46 @@
 #include "http_client.h"
-#include <Poco/Net/HTTPClientSession.h>
-#include <Poco/Net/HTTPRequest.h>
-#include <Poco/Net/HTTPResponse.h>
-#include <Poco/StreamCopier.h>
-#include <sstream>
-#include <iostream>
-
-void send_json_to_server(const nlohmann::json& data, const std::string& endpoint) {
-    try {
-        Poco::Net::HTTPClientSession session("89.169.154.118", 9090);
-        Poco::Net::HTTPRequest request(Poco::Net::HTTPRequest::HTTP_POST, endpoint, Poco::Net::HTTPMessage::HTTP_1_1);
-        std::string jsonStr = data.dump();
-
-        request.setContentType("application/json");
-        request.setContentLength(jsonStr.length());
-
-        std::ostream& os = session.sendRequest(request);
-        os << jsonStr;
-
-        Poco::Net::HTTPResponse response;
-        std::istream& is = session.receiveResponse(response);
-        std::stringstream responseStream;
-        Poco::StreamCopier::copyStream(is, responseStream);
 
 
-    } catch (const std::exception& e) {
-        std::cerr << "Error sending JSON: " << e.what() << "\n";
+void handle_server_response(QNetworkReply* reply){
+    if (reply->error() == QNetworkReply::NoError) {
+        QString response = reply->readAll();
+        qDebug() << "Ответ сервера: " << response;
+    } else {
+        qDebug() << "Ошибка: " << reply->errorString();
     }
+    reply->deleteLater();
 }
 
-void HttpClient::send_registration_data(const nlohmann::json& data) {
-    send_json_to_server(data, "/register");
-}
 
-void HttpClient::send_auth_data(const nlohmann::json& data) {
-    send_json_to_server(data, "/auth");
+int registrate(QString &nickname, QString &password){
+
+    QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+    QNetworkRequest request;
+    request.setUrl(QUrl("http://89.169.154.118:9090"));
+
+
+    QJsonObject json_data;
+    json_data["nickname"] = nickname;
+    json_data["password"] = password;
+
+    QJsonDocument json_doc(json_data);
+    QByteArray registration_data = json_doc.toJson();
+
+
+    qDebug() << "------------ отправка данных ------------";
+
+
+    QNetworkReply *reply = manager->post(request, registration_data);
+
+
+    connect(reply, &QNetworkReply::finished, this, [=](){
+        handle_server_response(reply);
+    });
+
+    connect(reply, &QNetworkReply::errorOccurred, this, [](QNetworkReply::NetworkError err) {
+        qDebug() << "Ошибка сети: " << err;
+    });
+
+    int status_code = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+    return status_code;
 }
