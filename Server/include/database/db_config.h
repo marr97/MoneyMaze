b838f9d8e686e7f84a2d4cfda887bee39c44f66a
alinatomilova <tomilova006@bk.ru>
2025-05-18 14:17:13 +0300

    update db_config.h

--- Server/include/database/db_config.h
+++ Server/include/database/db_config.h
@@ -4,5 +4,5 @@
 const std::string DB_NAME = "moneymaze_db";
 const std::string DB_USER = "moneymaze_user";
 const std::string DB_PASSWORD = "1111";
-const std::string DB_HOST = "http://89.169.154.118";
+const std::string DB_HOST = "89.169.154.118";
 const std::string DB_PORT = "9090";
