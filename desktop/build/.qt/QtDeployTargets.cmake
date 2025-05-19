d9336c77f0364f044db8e5cb65163f52c9dda332
alinatomilova <tomilova006@bk.ru>
2025-05-19 19:58:46 +0300

    delete build

--- desktop/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_money_maze_FILE /Users/alinatomilova/Desktop/MoneyMaze/Desktop/build/money_maze.app/Contents/MacOS/money_maze)
-set(__QT_DEPLOY_TARGET_money_maze_TYPE EXECUTABLE)
--- desktop/build/.qt/deploy_money_maze_62d3b4e93e.cmake
+++
@@ -1,7 +0,0 @@
-include(/Users/alinatomilova/Desktop/MoneyMaze/Desktop/build/.qt/QtDeploySupport.cmake)
-include("${CMAKE_CURRENT_LIST_DIR}/money_maze-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE money_maze.app
-)
