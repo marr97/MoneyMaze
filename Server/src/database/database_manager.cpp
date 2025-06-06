b5852fa3d419753067ef987571a4ca8da8bb9f9d
marr97 <mashashvets1234567890@gmail.com>
2025-06-06 17:57:08 +0300

    fixed DB2

--- Server/src/database/database_manager.cpp
+++ Server/src/database/database_manager.cpp
@@ -76,6 +76,27 @@ bool DatabaseManager::connect() {
                 );
             )");
             Logger::log(LogLevel::INFO, "Created table 'financial_profile'.");
+        } else {
+            // Проверка и исправление структуры
+            auto checkCol = txn.exec("SELECT 1 FROM information_schema.columns "
+                                    "WHERE table_name='financial_profile' "
+                                    "AND column_name='monthly_minimum'");
+            
+            if (checkCol.empty()) {
+                auto checkBadCol = txn.exec("SELECT 1 FROM information_schema.columns "
+                                            "WHERE table_name='financial_profile' "
+                                            "AND column_name='monthly_minimumINTEGER'");
+                
+                if (!checkBadCol.empty()) {
+                    txn.exec("ALTER TABLE financial_profile "
+                            "RENAME COLUMN \"monthly_minimumINTEGER\" TO \"monthly_minimum\"");
+                    Logger::log(LogLevel::INFO, "Fixed column name in financial_profile");
+                } else {
+                    txn.exec("ALTER TABLE financial_profile "
+                            "ADD COLUMN monthly_minimum INTEGER NOT NULL DEFAULT 0");
+                    Logger::log(LogLevel::WARNING, "Added missing column to financial_profile");
+                }
+            }
         }
         #pragma GCC diagnostic pop
 
